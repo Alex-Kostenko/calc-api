2019656abcd89ff9ff920809c570b76334f99551
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-03 15:04:35 +0200

    make coefficient required

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // import type { Core } from '@strapi/strapi';
 
+import { Core } from "@strapi/strapi";
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -16,5 +18,19 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  bootstrap({ strapi }: { strapi: Core.Strapi }) {
+    strapi.db.lifecycles.subscribe({
+      models: ["plugin::users-permissions.user"],
+      beforeUpdate({ params }) {
+        if (!params.data.coefficient) {
+          throw new Error("Coefficient must not be empty");
+        }
+      },
+      beforeCreate({ params }) {
+        if (!params.data.coefficient) {
+          throw new Error("Coefficient must not be empty");
+        }
+      },
+    });
+  },
 };
