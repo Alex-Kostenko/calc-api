b14fbc3e35453c023c3fc8ff4fc0ab4d2e01bbb8
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-07 16:23:31 +0200

    add function description

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -562,6 +562,7 @@ export interface ApiFormulaFormula extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    description: Schema.Attribute.Text;
     function: Schema.Attribute.Text & Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
