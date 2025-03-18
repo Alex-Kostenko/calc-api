045a5bc119275a09372061b148c2942de38fbf80
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-18 10:32:44 +0200

    override strapi endpoint for return all locations

--- src/api/location/controllers/location.ts
+++ src/api/location/controllers/location.ts
@@ -2,6 +2,26 @@
  * location controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::location.location');
+export default factories.createCoreController(
+  "api::location.location",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const data = await strapi.entityService.findMany(
+        "api::location.location",
+        {
+          limit: 1000,
+          populate: [
+            "state",
+            "auctions.auction_tax.tax",
+            "port.car_types",
+            "price_by_type",
+          ],
+        }
+      );
+
+      return { data };
+    },
+  })
+);
