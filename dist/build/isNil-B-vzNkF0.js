ee65b66f127e0ad4aa363a46050773375b501cba
oleksiychernenko286 <oleksiychernenko286@gmail.com>
2025-03-31 12:25:43 +0300

    update consts schema

--- dist/build/isNil-B-vzNkF0.js
+++
@@ -1,1 +0,0 @@
-import{g1 as r}from"./strapi-M_Ee3xdO.js";function t(i){return i==null}var o=t;const l=r(o);export{l as i};
--- dist/build/useOnce-EHSWW7FY-Cxp4Hdao.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./strapi-M_Ee3xdO.js";const r=e=>s.useEffect(e,t),t=[];export{r as u};
